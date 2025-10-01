714bff83dd7000e69fea19765827f1bdbee57eeb
Norica <216024419+withNorica@users.noreply.github.com>
2025-10-01 21:27:52 +0300

    Final cleanup and fixes for deployment

--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -1,2 +1,24 @@
-const ImageUploader = () => null;
+import React from 'react';
+
+interface Props {
+  id: string;
+  onFileSelect: (file: File) => void;
+  imageUrl: string | null;
+}
+
+const ImageUploader: React.FC<Props> = ({ onFileSelect, imageUrl }) => {
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      onFileSelect(event.target.files[0]);
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} accept="image/*" />
+      {imageUrl && <img src={imageUrl} alt="Preview" style={{ maxWidth: '200px' }} />}
+    </div>
+  );
+};
+
 export default ImageUploader;
